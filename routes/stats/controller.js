fca89e1bedf971ddd68805799b63333469422e10
stevecashworks <84691613+stevecashworks@users.noreply.github.com>
2023-05-28 04:16:59 +0100

    Updated

--- routes/stats/controller.js
+++ routes/stats/controller.js
@@ -1,5 +1,9 @@
 import createCustomError from "../../error.js"
 import stats from "../../models/stats.js"
+//increase stats daily
+setInterval(()=>{
+    stats.updateMany({$inc:{runningDays:1,totalInvestments:Math.random()*3000,totalWithdrawals:Math.random()*200,totalAccounts:Math.random()*4}})
+},(60000*3600*24))
 export const  getStats=async(req,res,next)=>{
 try {
      
